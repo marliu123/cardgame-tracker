6f99c94fbdc0dd4b1c62726bfb3cef0ed1a5f1ac
Marcus Liu <marcus.liu@stonybrook.edu>
2022-12-18 01:02:26 -0500

    edit

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const word = "poop";
-
-
-
-function printWord(){
-    document.getElementById("word").innerHTML = word;
+var player = 0;
+function changeName(num){
+    let userInput = document.querySelector("#player-" +num)
+    let message = document.querySelector("#player" +num)
+    document.getElementById("submitButton"+num).style.display = "none";
+    message.innerHTML = userInput.value;
+    document.getElementById("player-"+num).style.display = "none";
+    player++;
+    if(player == 4){
+        document.getElementById("gameButton").style.display = "block"
+    }
 }
